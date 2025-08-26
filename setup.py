04c7be08f9ac7c68ca6144b3764ffb85ca9e3435
Maziar Jamshidi <mz.jamshidi@gmail.com>
2025-08-26 16:30:34 -0600

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wildfire-fbp",
+    version="0.1.0",
+    author="Maziar Jamshidi",
+    packages=find_packages(exclude=("data",)),
+    install_requires=[
+        "numpy",
+        "rasterio"
+    ]
+)

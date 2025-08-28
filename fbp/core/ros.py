fix formula based of errata

fbp/core/ros.py
@@ -48,8 +48,9 @@ def _get_ros_params_m3(pdf: np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndar
     return a, b, c
 
 def _get_ros_params_m4(pdf: np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
-    """Eqs. 32, 33 & 34, FCFDG 1992"""
-    a = 140 * np.exp(-35.5 / pdf)
+    """Eqs. 32 (Errata), Wotton 2009"""
+    a = 140 * np.exp(-33.5 / pdf)
+    """Eqs. 33 & 34, FCFDG 1992"""
     b = 0.0404 * np.ones_like(pdf, dtype=float)
     c = 3.02 * np.exp(-0.00714 * pdf)
     return a, b, c
@@ -70,7 +71,15 @@ def _cf_formula(gc: np.ndarray):
                 0.005 * (np.exp(0.061 * gc) - 1),
                 0.176 + 0.02 * (gc - 58.8)
             )
+
+def _fF_formula(ffmc: np.ndarray):
+    FFMC_COEFFICIENT = 250 * 59.5 / 101
     
+    """Eq. 46, FCFDG 1992"""
+    m = FFMC_COEFFICIENT * (101 - ffmc) / (59.5 + ffmc)
+
+    """Eq. 45, FCFDG 1992"""
+    return 91.9 * np.exp(-0.1386 * m) * (1 + (m**5.31) / 4.93e7)
 
 def initial_rate_of_spread(
         fuel_map: np.ndarray,
@@ -142,13 +151,8 @@ def initial_spread_index(ffmc: np.ndarray, ws: np.ndarray) -> np.ndarray:
         ws: wind speed (km/h)
     """
     wsv = ws    # FIXME this probably not correct.
-    FFMC_COEFFICIENT = 250 * 59.5 / 101
-    
-    """Eq. 46, FCFDG 1992"""
-    m = FFMC_COEFFICIENT * (101 - ffmc) / (59.5 + ffmc)
 
-    """Eq. 45, FCFDG 1992"""
-    fF = 91.9 * np.exp(-0.1386 * m) * (1 + (m**5.31) / 4.93e7)
+    fF = _fF_formula(ffmc)
     
     """Eqs. 53 & 53a, FCFDG 1992: wsv: net effective wind speed"""
     fW = np.where(
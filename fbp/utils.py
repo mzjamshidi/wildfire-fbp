aad5fef095bed3716f24c24dde337de1df269ece
Maziar Jamshidi <mz.jamshidi@gmail.com>
2025-09-18 17:17:00 -0600

    add fmc to demo example

---
+++ fbp/utils.py
@@ -0,0 +1,18 @@
+import numpy as np
+from rasterio.warp import transform
+
+def make_grid(extent, shape):
+    x_min, x_max, y_min, y_max = extent
+    h, w = shape
+    dx = (x_max - x_min) / w
+    dy = (y_max - y_min) / h
+    x = np.linspace(x_min + dx/2, x_max - dx/2, w)
+    y = np.linspace(y_max - dy/2, y_min + dy/2, h)
+
+    X, Y = np.meshgrid(x, y)
+    return X, Y
+
+
+def to_lat_lon(easting: float, northing: float, src_crs: str):
+    lons, lats = transform(src_crs, "EPSG:4326", [easting], [northing])
+    return lons[0], lats[0]

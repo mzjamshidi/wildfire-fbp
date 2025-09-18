042a3149485e378ed83b84c4d65385996aef99c9
Maziar Jamshidi <mz.jamshidi@gmail.com>
2025-09-18 17:29:34 -0600

    minor improvements

--- fbp/fwi.py
+++ fbp/fwi.py
@@ -11,26 +11,27 @@ class FWIResults:
 
 class FWI:
     def __init__(self,
-                 latitude_south,
-                 latitude_north,
-                 longitude_east,
-                 longitude_west,
-                 shape,
+                 latitude_south: float,
+                 latitude_north: float,
+                 longitude_east: float,
+                 longitude_west: float,
+                 shape: tuple[int, int],
                  elevation = None,
                  ) -> None:
         
         h, w = shape
-        y = np.linspace(latitude_north, latitude_south, h)
-        x = np.linspace(longitude_east, longitude_west, w)
+        lat = np.linspace(latitude_north, latitude_south, h)
+        lon = np.linspace(longitude_east, longitude_west, w)
 
-        X, Y = np.meshgrid(x, y)
-        self.lat_arr = Y
-        self.lon_arr = X
+        self.lon_arr, self.lat_arr = np.meshgrid(lon, lat)
         self.elevation = elevation
 
-    def run(self, date: str):
-        dt = datetime.strptime(date, "%Y-%m-%d")
-        doy = dt.timetuple().tm_yday
+    def run(self, date: str | datetime):
+        
+        if isinstance(date, str):
+            date = datetime.strptime(date, "%Y-%m-%d")
+
+        doy = date.timetuple().tm_yday
         fmc = foliar_moisture_content(latitude=self.lat_arr,
                                 longitude=self.lon_arr,
                                 elevation=self.elevation,

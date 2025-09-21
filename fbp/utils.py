777728d71987e95e35be9b2508b0c14e830d79e9
Maziar Jamshidi <mz.jamshidi@gmail.com>
2025-09-21 12:02:41 -0600

    clean up and reorganize

--- fbp/utils.py
+++ fbp/utils.py
@@ -1,6 +1,8 @@
 import numpy as np
 from rasterio.warp import transform
 
+from fbp.constants import FBP_FUEL_DESC
+
 def make_grid(extent, shape):
     x_min, x_max, y_min, y_max = extent
     h, w = shape
@@ -16,3 +18,10 @@ def make_grid(extent, shape):
 def to_lat_lon(easting: float, northing: float, src_crs: str):
     lons, lats = transform(src_crs, "EPSG:4326", [easting], [northing])
     return lons[0], lats[0]
+
+
+def describe_fbp_fuel_types():
+    print("="*50)
+    for fuel, desc in FBP_FUEL_DESC.items():
+        print(f"{fuel:>5} : {desc:<50}")
+    print("="*50)
--- fbp/utils/info.py
+++
@@ -1,7 +0,0 @@
-from fbp.constants import FBP_FUEL_DESC
-
-def describe_fbp_fuel_types():
-    print("="*50)
-    for fuel, desc in FBP_FUEL_DESC.items():
-        print(f"{fuel:>5} : {desc:<50}")
-    print("="*50)
